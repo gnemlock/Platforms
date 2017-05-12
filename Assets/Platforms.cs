ee2a445ffb03560c019266421cc18f0bd3bd4db9
Matthew <gnemlock@gmail.com>
2017-05-12 22:44:36 +1000

    Unity Tutorial Testing
    Implemented functionality changes based of Unity tutorial "Make a Basic
    Platformer". Directed functionality is actually quite broken, but might
    be useful for reference.

---
+++ Assets/Platforms.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platforms.Utility
+{
+    public static class PlatformsTags
+    {
+        
+    }
+
+    public static class PlatformsStringFormats
+    {
+        
+    }
+
+    public static class PlatformsDebug
+    {
+    }
+}
--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -8,115 +8,165 @@ using UnityEngine;
 using UnityEditor;
 #endif
 
-/// <summary>Control for a 2D player.</summary>
-[RequireComponent(typeof(Rigidbody2D))] public class PlayerControl : MonoBehaviour
+namespace Platforms
 {
-    //TODO: Cap running velocity
-    //TODO: Test running velocity cap so it does not interfere if player has a higher base velocity;
-    //TODO: Enable grounding for jump
-    //TODO: Enable option for multi jump
-    //TODO: Enable interaction for climbing
-    //TODO: Determine InputManger source; if default, enable key down events from InputManager instead of Input.GetKeyDown
-    //TODO: Documentation
-    //TODO: RUN< JUMP< CLIMB
-    //TODO: Placeholder images to identify action
-    public float movementSpeed;
-    public float maxSpeed = 200f;
-    public float jumpPower;
-    public string horizontalAxisLabel;
+    using Dimensions = Platforms.Utility.PlayerControlDimensions;
+    using Tags = Platforms.Utility.PlayerControlTags;
 
-    public bool canOnlyRunWhenGrounded = false;
-    public bool runWithMomentum = true;
+    #if UNITY_EDITOR
+    using Tooltips = Platforms.Utility.PlayerControlTooltips;
+    #endif
 
-    [SerializeField] private Rigidbody2D rigidbody;
-    [SerializeField] private BoxCollider2D collider;
-    /// <summary>The distance from the center of the collider to the ground.</summary>
-    [SerializeField] private float distanceToGround;
-    /// <summary>The additional buffer between the edge of the player collider and the ground to 
-    /// allow for ground checking.</summary>
-    [SerializeField] private float groundingBuffer = 0.1f;
-
-    bool isGrounded
+    /// <summary>Control for a 2D player.</summary>
+    [RequireComponent(typeof(Rigidbody2D))] public class PlayerControl : MonoBehaviour
     {
-        get
+        //TODO: Cap running velocity
+        //TODO: Test running velocity cap so it does not interfere if player has a higher base velocity;
+        //TODO: Enable grounding for jump
+        //TODO: Enable option for multi jump
+        //TODO: Enable interaction for climbing
+        //TODO: Determine InputManger source; if default, enable key down events from InputManager instead of Input.GetKeyDown
+        //TODO: Documentation
+        //TODO: RUN< JUMP< CLIMB
+        //TODO: Placeholder images to identify action
+
+        [SerializeField][Tooltip(Tooltips.runForce)] private float runForce = 200f;
+        [SerializeField][Tooltip(Tooltips.jumpForce)] private float jumpForce = 1000f;
+        [SerializeField][Tooltip(Tooltips.maxRunVelocity)] private float maxRunVelocity = 200f;
+    
+        private Rigidbody2D rigidbody;
+        private BoxCollider2D collider;
+        /// <summary>The distance from the center of the collider to the ground.</summary>
+        private float distanceToGround;
+        private bool jumping = false;
+
+        /// <summary>Determines whether this <see cref="Platforms.PlayerControl"/> is currently 
+        /// grounded.</summary>
+        bool isGrounded
         {
-            return Physics2D.Raycast((Vector2)transform.position, Vector2.down, 
-                0.6f, 1 << 8);
+            get
+            {
+                // Perform a linecase between the current transform position, and the postion 
+                // where the ground should be, checking only against the ground layer.
+                // Return the results as a not null check.
+                return Physics2D.Linecast((Vector2)transform.position, 
+                    Vector2.down * (distanceToGround + Dimensions.groundBuffer), 
+                    1 << Dimensions.groundLayerIndex);
+            }
         }
-    }
-
-    private void Awake()
-    {
-        rigidbody = GetComponent<Rigidbody2D>();
-        collider = GetComponent<BoxCollider2D>();
-    }
-
-    private void Start()
-    {
-        distanceToGround = collider.bounds.extents.y;
-    }
-
-    // Fixed Update runs in synch with the physics system
-    private void FixedUpdate()
-    {
-        ApplyHorizontalInput();
-        ApplyVerticalInput();
-        CapSpeed();
-    }
-
-    void CapSpeed()
-    {
-        if(rigidbody.velocity.magnitude > maxSpeed)
+    
+        private void Awake()
         {
-            rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
+            // Set a reference to the local Rigidbody2D and BoxCollider2D components.
+            rigidbody = GetComponent<Rigidbody2D>();
+            collider = GetComponent<BoxCollider2D>();
         }
-    }
+    
+        private void Start()
+        {
+            // Set the distanceToGround as the lower y bound of the players collider.
+            distanceToGround = collider.bounds.extents.y;
+        }
+    
+        // Fixed Update runs in synch with the physics system
+        private void FixedUpdate()
+        {
+            ApplyHorizontalInput();
+            ApplyVerticalInput();
+        }
 
-    void ApplyHorizontalInput()
-    {
-        if(!canOnlyRunWhenGrounded || isGrounded)
+        private void Update()
+        {
+            if(Input.GetButtonDown(Tags.jumpInput) && isGrounded)
+            {
+                jumping = true;
+            }
+        }
+    
+        void ApplyHorizontalInput()
         {
             // If we are not set to only run when grounded, or are otherwise grounded, 
             // use the horizontal axis and movement speed to determine our current horizontal
             // movement, and apply fixed time scaling to it.
-            float runningSpeed = Input.GetAxis(horizontalAxisLabel) * movementSpeed;
-
+            float runInput = Input.GetAxis(Tags.horizontalInput) * runForce;
+            
             // If the horizontal movement does not have an empty x value,
-            if (runningSpeed != 0f)
+            if (runInput != 0f)
             {
                 // Move the player towards the horizontalMovement position by its rigidbody.
-                rigidbody.AddForce(Vector2.right * runningSpeed);
+                rigidbody.AddForce(Vector2.right * runInput);
+            }
+
+            if(Mathf.Abs(rigidbody.velocity.x) > maxRunVelocity)
+            {
+                rigidbody.velocity = new Vector2(Mathf.Sign(rigidbody.velocity.x) * maxRunVelocity, 
+                    rigidbody.velocity.y);
+            }
+        }
+    
+        void ApplyVerticalInput()
+        {
+            if(jumping)
+            {
+                rigidbody.AddForce(new Vector2(0f, jumpForce));
+                jumping = false;
+            }
+        }
+    
+        #if UNITY_EDITOR
+        public void FireTestRay()
+        {
+            RaycastHit2D hit 
+                = Physics2D.Raycast((Vector2)transform.position, Vector2.down, 0.6f, 1<<8);
+            Debug.Log(hit.collider.name);
+        }
+        #endif
+    }
+}
+
+namespace Platforms.Utility
+{
+    #if UNITY_EDITOR
+    [CustomEditor(typeof(PlayerControl))] class PlayerControlEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+    
+            if(GUILayout.Button(PlayerControlLabels.fireRay))
+            {
+                PlayerControl playerControl = target as PlayerControl;
+                playerControl.FireTestRay();
             }
         }
     }
+    #endif
 
-    void ApplyVerticalInput()
+    public static class PlayerControlDimensions
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rigidbody.AddForce(Vector2.up * jumpPower);
-        }
+        /// <summary>The additional distance to allow between the player and the floor, 
+        /// for detecting if the player is grounded.</summary>
+        public const float groundBuffer = 0.1f;
+        public const int groundLayerIndex = 8;
+    }
+
+    public static class PlayerControlTags
+    {
+        public const string horizontalInput = "Run";
+        public const string jumpInput = "Jump";
     }
 
     #if UNITY_EDITOR
-    public void FireTestRay()
+    public static class PlayerControlTooltips
     {
-        RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position, Vector2.down, 0.6f, 1<<8);
-        Debug.Log(hit.collider.name);
+        public const string runForce = "The force to apply when the player is running.";
+        public const string jumpForce = "The force to apply when the player jumps.";
+        public const string maxRunVelocity = "The maximum velocity to allow for running";
+    }
+
+    public static class PlayerControlLabels
+    {
+        public const string fireRay = "Fire Ray";
     }
     #endif
-}
-
-[CustomEditor(typeof(PlayerControl))] class PlayerControlEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
-
-        if(GUILayout.Button("Fire Ray"))
-        {
-            PlayerControl playerControl = target as PlayerControl;
-            playerControl.FireTestRay();
-        }
-    }
 }
